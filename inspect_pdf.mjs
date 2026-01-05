1d19bf6bb736e2e50ac56cceafb9ad68e3db70d9
Sebastian <sebastiandanielgosti8@gmail.com>
2026-01-05 19:52:49 -0300

    Final Cleanup & Release Candidate: Optimized assets and removed temporary files

--- inspect_pdf.mjs
+++
@@ -1,9 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const pdf = require('pdf-parse');
-
-console.log("Type:", typeof pdf);
-console.log("Has default:", 'default' in pdf);
-console.log("Default type:", typeof pdf.default);
-const fs = require('fs');
-fs.writeFileSync('keys_full.txt', JSON.stringify(Object.keys(pdf), null, 2));
